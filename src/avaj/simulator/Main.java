867c654213a2495f2888d47503d573b69306a716
Boris Goncharov <bora4bora@gmail.com>
2019-11-20 14:48:42 -0800

    main class

--- src/avaj/simulator/Main.java
+++ src/avaj/simulator/Main.java
@@ -4,8 +4,39 @@ import java.io.*;
 
 public class Main {
 
+    private int n;
+    WeatherTower weatherTower = new WeatherTower();
+
+    private void parseFile(String filename) {
+        try {
+            int i = 1;
+            FileReader file = new FileReader(filename);
+            BufferedReader in = new BufferedReader(file);
+            String s = in.readLine();
+            n = Integer.parseInt(s);
+            while ((s = in.readLine()) != null) {
+                i++;
+                String[] arguments = s.split(" ");
+                Flyable flyable = AircraftFactory.newAircraft(arguments[0], arguments[1],
+                        Integer.parseInt(arguments[2]), Integer.parseInt(arguments[3]),
+                        Integer.parseInt(arguments[4]));
+                flyable.registerTower(weatherTower);
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+        catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("It works!");
-       // Launcher launch = new Launcher();
+        if (args.length == 0) {
+            System.err.println("Put the file name");
+            System.exit(0);
+        }
+        Main m = new Main();
+        m.parseFile(args[0]);
     }
 }
